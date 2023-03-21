ee6433e43feb6a26f2b05944bfb8cae81144ce10
themw123 <80266862+themw123@users.noreply.github.com>
2023-03-22 00:40:31 +0100

    n

--- syncNews/index.php
+++ syncNews/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once('./vendor/autoload.php');
 require_once("/var/www/html/config.php");
 require_once("/var/www/html/klassen/DbFunctions.inc.php");
 require_once("/var/www/html/klassen/Request.inc.php");
