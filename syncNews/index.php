c73d3dad7cc7fab386b23b194b680bfca37e5737
themw <marvin.w6@web.de>
2023-03-23 02:34:32 +0100

    n

---
+++ syncNews/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once('./vendor/autoload.php');
+
+require_once("/var/www/html/klassen/DbFunctions.inc.php");
+require_once("/var/www/html/klassen/Request.inc.php");
+
+require_once("./config.php");
+require_once("./News.inc.php");
+
+
+
+
+$news = new News();
