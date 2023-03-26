071c54b1c031a41900185be37812de4a8bcbe3d0
themw <marvin.w6@web.de>
2023-03-26 23:55:16 +0200

    n

--- syncNews/index.php
+++ syncNews/index.php
@@ -1,10 +1,11 @@
 <?php
+//achtung immer absolute pfade angeben sonnst probleme!!!!
 require_once("/var/www/html/vendor/autoload.php");
 require_once("/var/www/html/klassen/DbFunctions.inc.php");
 require_once("/var/www/html/klassen/Request.inc.php");
 
-require_once("./config.php");
-require_once("./News.inc.php");
+require_once("/home/config.php");
+require_once("/home/News.inc.php");
 
 
 
