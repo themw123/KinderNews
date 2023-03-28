cfdcd2b45f266ffb66ddc3472abbf5493c0d8e75
themw123 <80266862+themw123@users.noreply.github.com>
2023-03-28 21:16:32 +0200

    n

--- smarty/templates_c/f22589f8f1dd810b744db9b82763e58b44b7b688_0.file.newsArticle.tpl.php
+++ smarty/templates_c/f22589f8f1dd810b744db9b82763e58b44b7b688_0.file.newsArticle.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.0, created on 2023-03-28 21:01:14
+/* Smarty version 4.2.0, created on 2023-03-28 21:15:47
   from 'C:\xampp\htdocs\Projekte\KinderNews\smarty\templates\newsArticle.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.0',
-  'unifunc' => 'content_6423397a359d58_38735081',
+  'unifunc' => 'content_64233ce39588f3_94152467',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f22589f8f1dd810b744db9b82763e58b44b7b688' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Projekte\\KinderNews\\smarty\\templates\\newsArticle.tpl',
-      1 => 1680030061,
+      1 => 1680030942,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:navbar.tpl' => 1,
   ),
 ),false)) {
-function content_6423397a359d58_38735081 (Smarty_Internal_Template $_smarty_tpl) {
+function content_64233ce39588f3_94152467 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE HTML>
 <html>
 
@@ -46,18 +46,23 @@ function content_6423397a359d58_38735081 (Smarty_Internal_Template $_smarty_tpl)
 ?>
     <div class="d-flex flex-column justify-content-center align-items-center">
         <div class="backgr d-flex flex-column justify-content-center align-items-center">
-            <div class="bildcontainer container-fluid">
+            <?php if ($_smarty_tpl->tpl_vars['newsArticle']->value["bild_url"] == null) {?>
+                <div class="bildcontainerError container-fluid">
 
-                <?php if ($_smarty_tpl->tpl_vars['newsArticle']->value["bild_url"] == null) {?>
                     <img class="bild custom-shadow"
                         src="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
                         class="card-img-top" alt="">
-                <?php } else { ?>
+                </div>
+
+            <?php } else { ?>
+                <div class="bildcontainer container-fluid">
+
                     <img class="bild custom-shadow" src=<?php echo $_smarty_tpl->tpl_vars['newsArticle']->value["bild_url"];?>
  class="card-img-top" alt=""
                         onerror="this.onerror=null; this.src='https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'; ">
-                <?php }?>
-            </div>
+                </div>
+
+            <?php }?>
 
             <p class="ptitle">
                 <?php echo $_smarty_tpl->tpl_vars['newsArticle']->value["uebersetzter_titel"];?>
