3f4b04bb293cc7bcdd3587b643406831de2f2145
themw123 <marvin.w6@web.de>
2023-03-30 11:28:53 +0200

    n

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,6 @@ if ('serviceWorker' in navigator) {
 }
 
 
-
 const cacheName = 'meine-pwa-cache';
 const filesToCache = [
 
@@ -37,6 +36,64 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+
+
+
+
+
+
+
+//push
+
+// Funktion zum Anfordern der Benachrichtigungsberechtigung
+function requestNotificationPermission() {
+  // Überprüfen, ob die Browser-Plattform die Notification-API unterstützt
+  if ('Notification' in window) {
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        console.log('Benachrichtigungsberechtigung erteilt');
+      } else {
+        console.warn('Benachrichtigungsberechtigung nicht erteilt');
+      }
+    });
+  }
+}
+
+// Ereignis-Handler-Funktion, die auf eine Benutzeraktion reagiert und die Funktion zum Anfordern der Benachrichtigungsberechtigung aufruft
+function handleNotificationPermission() {
+  // Überprüfen, ob der Browser die Notification-API unterstützt
+  if ('Notification' in window) {
+    // Überprüfen, ob der Benutzer bereits die Benachrichtigungsberechtigung erteilt hat
+    if (Notification.permission === 'granted') {
+      console.log('Benachrichtigungsberechtigung erteilt');
+      sendNotification(); // Hier wird die Push-Benachrichtigung geplant
+    } else {
+      // Anfordern der Benachrichtigungsberechtigung
+      requestNotificationPermission();
+    }
+  }
+}
+
+// Funktion zum Senden der Push-Benachrichtigung
+function sendNotification() {
+  const notificationTitle = 'Zeit für KinderNews!';
+  const notificationOptions = {
+    body: 'Es reich mit TikTok! Zeit für die neusten KinderNews!',
+    icon: '/img/app.png',
+    vibrate: [200, 100, 200, 100, 200, 100, 200]
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+}
+
+
+setInterval(sendNotification, 60000);
+
+
+
+// Aufruf der Funktionen zum Anfordern der Benachrichtigungsberechtigung und zum Planen der Push-Benachrichtigung
+//requestNotificationPermission();
+document.querySelector('.navbar-toggler-icon').addEventListener('click', handleNotificationPermission);
+//scheduleNotification();
 
 
 
