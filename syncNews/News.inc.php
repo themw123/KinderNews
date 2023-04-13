0070fec406e99d63e26a76c5f1c923b71998fec5
themw123 <marvin.w6@web.de>
2023-04-13 16:55:53 +0200

    n

--- syncNews/News.inc.php
+++ syncNews/News.inc.php
@@ -145,110 +145,114 @@ class News
         $counter = 1;
         foreach ($this->news as $article) {
 
-
             try {
 
-                
-            $title = $article['title'];
-            $text = $article['text'];
+                $title = $article['title'];
+                $text = $article['text'];
 
-            //als eingabe ungefähr 1 1/2 Word Seiten, also 7366 Zeichen (≈2800 Token) und als Ausgabe ungefähr 3/4 Word Seite = 3159 Zeichen (≈1200 Token)
-            //mit 1 1/2 Word Seiten als Eingabe und 3/4 Word Seite als Ausgabe, sind es dann 4000 Token 
-            //quelle https://platform.openai.com/tokenizer
+                //als eingabe ungefähr 1 1/2 Word Seiten, also 7366 Zeichen (≈2800 Token) und als Ausgabe ungefähr 3/4 Word Seite = 3159 Zeichen (≈1200 Token)
+                //mit 1 1/2 Word Seiten als Eingabe und 3/4 Word Seite als Ausgabe, sind es dann 4000 Token 
+                //quelle https://platform.openai.com/tokenizer
 
-            //wenn zu groß, dann kürzen.
-            //abzüglich ungefähr 1900 für input promt also 7366 - 1900 = 5466
-            $maxLength = 5466;
-            if (strlen($text) > $maxLength) {
-                $text = substr($text, 0, $maxLength);
-            }
+                //wenn zu groß, dann kürzen.
+                //abzüglich ungefähr 1900 für input promt also 7366 - 1900 = 5466
+                $maxLength = 5466;
+                if (strlen($text) > $maxLength) {
+                    $text = substr($text, 0, $maxLength);
+                }
 
-            $response = Request::requestTranslate($title, $text);
+                $response = Request::requestTranslate($title, $text);
 
-            if ($response === "private_error") {
-                $this->placeholder();
+                if ($response === "private_error") {
+                    $this->placeholder();
+                    $counter++;
+                }
+
+                if ($response === false) {
+                    $this->placeholder();
+                    $counter++;
+                    continue;
+                }
+
+                $json = json_decode($response);
+
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    $this->placeholder();
+                    $counter++;
+                    continue;
+                }
+
+                if (isset($json->error) && !empty($json->error)) {
+                    $this->placeholder();
+                    $counter++;
+                    continue;
+                }
+
+                $responseText = $json->{"choices"}[0]->{"message"}->{"content"};
+                $responseText = trim($responseText);
+
+                $myJson = json_decode($responseText);
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    $this->placeholder();
+                    $counter++;
+                    continue;
+                }
+                $translatedTitle = $myJson->{"title"};
+                $translatedText = $myJson->{"text"};
+                $preview = substr($translatedText, 0, 167) . "...";
+
+                if (isset($myJson->{"question1"})) {
+                    $question1 = $myJson->{"question1"};
+                } else {
+                    $question1 = "error";
+                }
+                if (isset($myJson->{"question2"})) {
+                    $question2 = $myJson->{"question2"};
+                } else {
+                    $question2 = "error";
+                }
+                if (isset($myJson->{"question3"})) {
+                    $question3 = $myJson->{"question3"};
+                } else {
+                    $question3 = "error";
+                }
+
+                if (isset($myJson->{"answer1"})) {
+                    $answer1 = $myJson->{"answer1"};
+                } else {
+                    $answer1 = "error";
+                }
+                if (isset($myJson->{"answer2"})) {
+                    $answer2 = $myJson->{"answer2"};
+                } else {
+                    $answer2 = "error";
+                }
+                if (isset($myJson->{"answer3"})) {
+                    $answer3 = $myJson->{"answer3"};
+                } else {
+                    $answer3 = "error";
+                }
+
+                $this->newsTranslated[] = array(
+                    'title' => $translatedTitle,
+                    'text' => $translatedText,
+                    'preview' => $preview,
+                    'question1' => $question1,
+                    'question2' => $question2,
+                    'question3' => $question3,
+                    'answer1' => $answer1,
+                    'answer2' => $answer2,
+                    'answer3' => $answer3
+                );
+
+
                 $counter++;
-            }
-
-            if ($response === false) {
+            } catch (Exception $e) {
+                Logs::addError("Unbekannter Fehler beim übersetzten der $counter. von " . count($this->news) . ".");
                 $this->placeholder();
                 $counter++;
                 continue;
             }
-
-            $json = json_decode($response);
-
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                $this->placeholder();
-                $counter++;
-                continue;
-            }
-
-            if (isset($json->error) && !empty($json->error)) {
-                $this->placeholder();
-                $counter++;
-                continue;
-            }
-
-            $responseText = $json->{"choices"}[0]->{"message"}->{"content"};
-            $responseText = trim($responseText);
-
-            $myJson = json_decode($responseText);
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                $this->placeholder();
-                $counter++;
-                continue;
-            }
-            $translatedTitle = $myJson->{"title"};
-            $translatedText = $myJson->{"text"};
-            $preview = substr($translatedText, 0, 167) . "...";
-
-            if (isset($myJson->{"question1"})) {
-                $question1 = $myJson->{"question1"};
-            } else {
-                $question1 = "error";
-            }
-            if (isset($myJson->{"question2"})) {
-                $question2 = $myJson->{"question2"};
-            } else {
-                $question2 = "error";
-            }
-            if (isset($myJson->{"question3"})) {
-                $question3 = $myJson->{"question3"};
-            } else {
-                $question3 = "error";
-            }
-
-            if (isset($myJson->{"answer1"})) {
-                $answer1 = $myJson->{"answer1"};
-            } else {
-                $answer1 = "error";
-            }
-            if (isset($myJson->{"answer2"})) {
-                $answer2 = $myJson->{"answer2"};
-            } else {
-                $answer2 = "error";
-            }
-            if (isset($myJson->{"answer3"})) {
-                $answer3 = $myJson->{"answer3"};
-            } else {
-                $answer3 = "error";
-            }
-
-            $this->newsTranslated[] = array(
-                'title' => $translatedTitle,
-                'text' => $translatedText,
-                'preview' => $preview,
-                'question1' => $question1,
-                'question2' => $question2,
-                'question3' => $question3,
-                'answer1' => $answer1,
-                'answer2' => $answer2,
-                'answer3' => $answer3
-            );
-
-
-            $counter++;
         }
 
         if ($counter == 1) {
