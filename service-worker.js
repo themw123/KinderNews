65c45585f754d44e215e23e8aa3487068fde8ad7
themw123 <themwudo123@gmail.com>
2023-04-25 13:35:57 +0200

    n

--- service-worker.js
+++ service-worker.js
@@ -8,27 +8,3 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-
-// setze die Zeit für die erste Benachrichtigung auf die aktuelle Zeit plus 1 Minute
-localStorage.setItem('notificationTime', new Date().getTime() + 60 * 1000);
-
-// Timer-Callback, um die Benachrichtigung zu senden
-function sendNotification() {
-  var notification = new Notification('Titel der Benachrichtigung', {
-    body: 'Nachrichteninhalt',
-    icon: '/img/app.png'
-  });
-  
-  // aktualisiere die Benachrichtigungszeit auf die nächste Minute
-  localStorage.setItem('notificationTime', new Date().getTime() + 60 * 1000);
-}
-
-// Timer, um die Benachrichtigung jede Minute zu senden
-setInterval(function() {
-  var notificationTime = localStorage.getItem('notificationTime');
-  
-  // wenn die aktuelle Zeit größer oder gleich der Benachrichtigungszeit ist, sende die Benachrichtigung
-  if (new Date().getTime() >= notificationTime) {
-    sendNotification();
-  }
-}, 1000); // überprüfe alle 1 Sekunde, ob es Zeit für eine Benachrichtigung ist
