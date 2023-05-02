e23c1d9fbd34fa94e133238c02eb7f9a0fe0b60b
themw123 <80266862+themw123@users.noreply.github.com>
2023-05-02 17:32:28 +0200

    n

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '9d9206a50eba962c4b3032f8f3efe196103abe62',
+        'reference' => '7a1eb2b9da7e2a7dc9234fff035754e2724370bd',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '9d9206a50eba962c4b3032f8f3efe196103abe62',
+            'reference' => '7a1eb2b9da7e2a7dc9234fff035754e2724370bd',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
