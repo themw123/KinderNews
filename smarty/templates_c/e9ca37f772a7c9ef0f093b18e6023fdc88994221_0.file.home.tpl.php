bdc31b75ab181b116da2dbfa49d2e1319d9fb659
themw <marvin.w6@web.de>
2023-06-05 02:21:58 +0200

    n

--- smarty/templates_c/e9ca37f772a7c9ef0f093b18e6023fdc88994221_0.file.home.tpl.php
+++ smarty/templates_c/e9ca37f772a7c9ef0f093b18e6023fdc88994221_0.file.home.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-06-05 01:47:28
+/* Smarty version 4.3.1, created on 2023-06-05 02:16:48
   from 'C:\xampp\htdocs\Projekte\KinderNews\smarty\templates\home.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_647d2290699244_40670387',
+  'unifunc' => 'content_647d2970da7aa6_37260192',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e9ca37f772a7c9ef0f093b18e6023fdc88994221' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Projekte\\KinderNews\\smarty\\templates\\home.tpl',
-      1 => 1685920675,
+      1 => 1685922660,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:navbar.tpl' => 1,
   ),
 ),false)) {
-function content_647d2290699244_40670387 (Smarty_Internal_Template $_smarty_tpl) {
+function content_647d2970da7aa6_37260192 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE HTML>
 <html>
 
