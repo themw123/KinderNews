4e83485431265c71b779ca10e6d056398b4f115d
themw123 <marvin.walczak@stud.hs-bochum.de>
2023-06-21 14:35:43 +0200

    n

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,105 +1,85 @@
 <?php return array(
     'root' => array(
+        'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-<<<<<<< HEAD
-        'reference' => 'd62b26fac8aa8432bd3e096adaa51ea4d041e07c',
-=======
->>>>>>> 8603030 (wildes zwischenupdate damit ich pullen kann)
+        'reference' => '9beab35d00dd0da07005449db94fb6155520cd03',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '605cefa11993debe794502a59ac3da1748508548',
-        'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-<<<<<<< HEAD
-            'reference' => 'd62b26fac8aa8432bd3e096adaa51ea4d041e07c',
-=======
->>>>>>> 8603030 (wildes zwischenupdate damit ich pullen kann)
+            'reference' => '9beab35d00dd0da07005449db94fb6155520cd03',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '605cefa11993debe794502a59ac3da1748508548',
             'dev_requirement' => false,
         ),
         'brick/math' => array(
             'pretty_version' => '0.11.0',
             'version' => '0.11.0.0',
+            'reference' => '0ad82ce168c82ba30d1c01ec86116ab52f589478',
             'type' => 'library',
             'install_path' => __DIR__ . '/../brick/math',
             'aliases' => array(),
-            'reference' => '0ad82ce168c82ba30d1c01ec86116ab52f589478',
             'dev_requirement' => false,
         ),
         'guzzlehttp/guzzle' => array(
-<<<<<<< HEAD
             'pretty_version' => '7.7.0',
             'version' => '7.7.0.0',
             'reference' => 'fb7566caccf22d74d1ab270de3551f72a58399f5',
-=======
-            'pretty_version' => '7.5.1',
-            'version' => '7.5.1.0',
->>>>>>> 8603030 (wildes zwischenupdate damit ich pullen kann)
             'type' => 'library',
             'install_path' => __DIR__ . '/../guzzlehttp/guzzle',
             'aliases' => array(),
-            'reference' => 'b964ca597e86b752cd994f27293e9fa6b6a95ed9',
             'dev_requirement' => false,
         ),
         'guzzlehttp/promises' => array(
-<<<<<<< HEAD
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
             'reference' => '3a494dc7dc1d7d12e511890177ae2d0e6c107da6',
-=======
-            'pretty_version' => '1.5.2',
-            'version' => '1.5.2.0',
->>>>>>> 8603030 (wildes zwischenupdate damit ich pullen kann)
             'type' => 'library',
             'install_path' => __DIR__ . '/../guzzlehttp/promises',
             'aliases' => array(),
-            'reference' => 'b94b2807d85443f9719887892882d0329d1e2598',
             'dev_requirement' => false,
         ),
         'guzzlehttp/psr7' => array(
             'pretty_version' => '2.5.0',
             'version' => '2.5.0.0',
+            'reference' => 'b635f279edd83fc275f822a1188157ffea568ff6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../guzzlehttp/psr7',
             'aliases' => array(),
-            'reference' => 'b635f279edd83fc275f822a1188157ffea568ff6',
             'dev_requirement' => false,
         ),
         'haozi-team/chatgpt-php' => array(
             'pretty_version' => '2.0.4',
             'version' => '2.0.4.0',
+            'reference' => 'd513bdbf3f6a972fc0552d1a2cea7d9e5c4a4f34',
             'type' => 'library',
             'install_path' => __DIR__ . '/../haozi-team/chatgpt-php',
             'aliases' => array(),
-            'reference' => 'd513bdbf3f6a972fc0552d1a2cea7d9e5c4a4f34',
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
             'pretty_version' => 'v6.8.0',
             'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
-            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
             'dev_requirement' => false,
         ),
         'psr/http-client' => array(
             'pretty_version' => '1.0.2',
             'version' => '1.0.2.0',
+            'reference' => '0955afe48220520692d2d09f7ab7e0f93ffd6a31',
             'type' => 'library',
             'install_path' => __DIR__ . '/../psr/http-client',
             'aliases' => array(),
-            'reference' => '0955afe48220520692d2d09f7ab7e0f93ffd6a31',
             'dev_requirement' => false,
         ),
         'psr/http-client-implementation' => array(
@@ -111,10 +91,10 @@
         'psr/http-factory' => array(
             'pretty_version' => '1.0.2',
             'version' => '1.0.2.0',
+            'reference' => 'e616d01114759c4c489f93b099585439f795fe35',
             'type' => 'library',
             'install_path' => __DIR__ . '/../psr/http-factory',
             'aliases' => array(),
-            'reference' => 'e616d01114759c4c489f93b099585439f795fe35',
             'dev_requirement' => false,
         ),
         'psr/http-factory-implementation' => array(
@@ -126,10 +106,10 @@
         'psr/http-message' => array(
             'pretty_version' => '2.0',
             'version' => '2.0.0.0',
+            'reference' => '402d35bcb92c70c026d1a6a9883f06b2ead23d71',
             'type' => 'library',
             'install_path' => __DIR__ . '/../psr/http-message',
             'aliases' => array(),
-            'reference' => '402d35bcb92c70c026d1a6a9883f06b2ead23d71',
             'dev_requirement' => false,
         ),
         'psr/http-message-implementation' => array(
@@ -141,28 +121,28 @@
         'ralouphie/getallheaders' => array(
             'pretty_version' => '3.0.3',
             'version' => '3.0.3.0',
+            'reference' => '120b605dfeb996808c31b6477290a714d356e822',
             'type' => 'library',
             'install_path' => __DIR__ . '/../ralouphie/getallheaders',
             'aliases' => array(),
-            'reference' => '120b605dfeb996808c31b6477290a714d356e822',
             'dev_requirement' => false,
         ),
         'ramsey/collection' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
+            'reference' => 'a4b48764bfbb8f3a6a4d1aeb1a35bb5e9ecac4a5',
             'type' => 'library',
             'install_path' => __DIR__ . '/../ramsey/collection',
             'aliases' => array(),
-            'reference' => 'a4b48764bfbb8f3a6a4d1aeb1a35bb5e9ecac4a5',
             'dev_requirement' => false,
         ),
         'ramsey/uuid' => array(
             'pretty_version' => '4.7.4',
             'version' => '4.7.4.0',
+            'reference' => '60a4c63ab724854332900504274f6150ff26d286',
             'type' => 'library',
             'install_path' => __DIR__ . '/../ramsey/uuid',
             'aliases' => array(),
-            'reference' => '60a4c63ab724854332900504274f6150ff26d286',
             'dev_requirement' => false,
         ),
         'rhumsaa/uuid' => array(
@@ -174,25 +154,19 @@
         'smarty/smarty' => array(
             'pretty_version' => 'v4.3.1',
             'version' => '4.3.1.0',
+            'reference' => 'e28cb0915b4e3749bf57d4ebae2984e25395cfe5',
             'type' => 'library',
             'install_path' => __DIR__ . '/../smarty/smarty',
             'aliases' => array(),
-            'reference' => 'e28cb0915b4e3749bf57d4ebae2984e25395cfe5',
             'dev_requirement' => false,
         ),
         'symfony/deprecation-contracts' => array(
-<<<<<<< HEAD
             'pretty_version' => 'v3.3.0',
             'version' => '3.3.0.0',
             'reference' => '7c3aff79d10325257a001fcf92d991f24fc967cf',
-=======
-            'pretty_version' => 'v3.2.1',
-            'version' => '3.2.1.0',
->>>>>>> 8603030 (wildes zwischenupdate damit ich pullen kann)
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/deprecation-contracts',
             'aliases' => array(),
-            'reference' => 'e2d1534420bd723d0ef5aec58a22c5fe60ce6f5e',
             'dev_requirement' => false,
         ),
     ),
